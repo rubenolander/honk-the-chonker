59913a873ae809d66798ba44a27e338cc5b68351
RubenOlander <rubenolander@gmail.com>
2022-12-04 19:53:31 +0100

    cats flip now

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
+// HECKIN SELECTION SECTION
 const CHONKER = document.querySelector('.chonk');
 const chonkHeader = document.querySelector('h1');
 const body = document.querySelector('body');
 const song = document.querySelector('.fruttimango');
 const honk = document.querySelector('.honk');
+const honkThese = document.querySelectorAll('.honk-me');
+let topHonk = document.querySelector('.oneHonk');
+
+
+//Data and preloads
 song.preload = "auto";
 honk.playbackrate = 1.2;
+const flipValueArray = ['scaleX(1)', 'scaleX(-1)'];
+let i = 0;
 
+//Why is this an array of objects even.
+const cats = [ 
+    topCat = {
+    placement: "top",
+    url: "/media/st,small,507x507-pad,600x600,f8f8f8.jpg",
+    },
+    bottomCat = {
+        placement: "bottom",
+        url: "/media/st,small,507x507-pad,600x600,f8f8f8.jpg",
+    },
+    gifCat = {
+        placement: "bottomLeft",
+        url: "/media/cat.gif"
+    }
+];
+
+
+//Do's
 CHONKER.addEventListener('click', () => {
     song.play()
-    CHONKER.classList.add('bouncyChonker');
-    chonkHeader.classList.add('spinnyChonker');
+    CHONKER.classList.add('bouncy-Chonker');
+    chonkHeader.classList.add('spinny-Chonker');
 });
-if (song.ended == false){
-    console.log(song.ended);
-    body.addEventListener('click', () => {
-        honk.play();
+honkThese.forEach(honkThis => {
+    honkThis.addEventListener('click', () => {
+        honk.play();    
+        
+        honkThis.style.transform=flipValueArray[i++];
+        if (i >= flipValueArray.length){
+            i = 0;
+        }
     })
-}
+});
