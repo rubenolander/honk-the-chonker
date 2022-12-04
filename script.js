6537b2a138b5504b5b72ec9d5ebbad6b564c3438
RubenOlander <rubenolander@gmail.com>
2022-12-04 09:22:29 +0100

    sounds added

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-const CHONKER = document.querySelector('h1');
+const CHONKER = document.querySelector('.chonk');
+const chonkHeader = document.querySelector('h1');
 const body = document.querySelector('body');
+const song = document.querySelector('.fruttimango');
+const honk = document.querySelector('.honk');
+song.preload = "auto";
+honk.playbackrate = 1.2;
 
+CHONKER.addEventListener('click', () => {
+    song.play()
+    CHONKER.classList.add('bouncyChonker');
+    chonkHeader.classList.add('spinnyChonker');
+});
+if (song.ended == false){
+    console.log(song.ended);
+    body.addEventListener('click', () => {
+        honk.play();
+    })
+}
