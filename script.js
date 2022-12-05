415eb39d9cc7c266baa0604d900eaf3fa29fbc3b
rubenolander <rubenolander@gmail.com>
2022-12-05 10:31:05 +0100

    Added final cats, and swivel effect on wheel
    Pretty much done. Will add some flair though.

--- script.js
+++ script.js
@@ -1,49 +1,56 @@
 // HECKIN SELECTION SECTION
 const CHONKER = document.querySelector('.chonk');
 const chonkHeader = document.querySelector('h1');
-const body = document.querySelector('body');
 const song = document.querySelector('.fruttimango');
 const honk = document.querySelector('.honk');
 const honkThese = document.querySelectorAll('.honk-me');
-let topHonk = document.querySelector('.oneHonk');
-
+const topHonk = document.querySelector('.first-chonker');
+const bottomHonk = document.querySelector('.second-chonker');
 
 //Data and preloads
-song.preload = "auto";
+song.preload = 'auto';
 honk.playbackrate = 1.2;
 const flipValueArray = ['scaleX(1)', 'scaleX(-1)'];
 let i = 0;
 
-//Why is this an array of objects even.
-const cats = [ 
-    topCat = {
-    placement: "top",
-    url: "/media/st,small,507x507-pad,600x600,f8f8f8.jpg",
-    },
-    bottomCat = {
-        placement: "bottom",
-        url: "/media/st,small,507x507-pad,600x600,f8f8f8.jpg",
-    },
-    gifCat = {
-        placement: "bottomLeft",
-        url: "/media/cat.gif"
-    }
-];
-
-
 //Do's
 CHONKER.addEventListener('click', () => {
-    song.play()
-    CHONKER.classList.add('bouncy-Chonker');
-    chonkHeader.classList.add('spinny-Chonker');
+  song.play();
+  chonkHeader.classList.add('spinny-Chonker');
+  CHONKER.classList.add('bouncy-Chonker');
 });
-honkThese.forEach(honkThis => {
-    honkThis.addEventListener('click', () => {
-        honk.play();    
-        
-        honkThis.style.transform=flipValueArray[i++];
-        if (i >= flipValueArray.length){
-            i = 0;
-        }
-    })
+honkThese.forEach((honkThis) => {
+  honkThis.addEventListener('click', () => {
+    honk.play();
+  });
 });
+
+// I don't know why but it seems I need one "wheel" variable for each picture.
+// And I can't use a foreach loop to make both pictures listen to 'wheel'.
+// I also can't have too close of a interval (10ms seems to do the trick).
+// That's why this code is repeating.
+let wheel1 = false;
+let wheel2 = false;
+window.addEventListener('wheel', () => {
+  wheel1 = true;
+  wheel2 = true;
+});
+setInterval(() => {
+  if (wheel1) {
+    wheel1 = false;
+    topHonk.style.transform = flipValueArray[i++];
+    if (i >= flipValueArray.length) {
+      i = 0;
+    }
+  }
+}, 40);
+
+setInterval(() => {
+  if (wheel2) {
+    wheel2 = false;
+    bottomHonk.style.transform = flipValueArray[i++];
+    if (i >= flipValueArray.length) {
+      i = 0;
+    }
+  }
+}, 50);
