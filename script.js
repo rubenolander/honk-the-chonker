ab9ca7790d221752b3ad2a426c563c45c26fffe4
rubenolander <rubenolander@gmail.com>
2022-12-10 22:12:46 +0100

    some flashy additions. done now

--- script.js
+++ script.js
@@ -4,18 +4,24 @@ const chonkHeader = document.querySelector('h1');
 const song = document.querySelector('.fruttimango');
 const honk = document.querySelector('.honk');
 const honkThese = document.querySelectorAll('.honk-me');
+const topContainer = document.querySelector('.first-container');
 const topHonk = document.querySelector('.first-chonker');
+const bottomContainer = document.querySelector('.second-container');
 const bottomHonk = document.querySelector('.second-chonker');
+const dogeDance = document.querySelector('.doge-dance');
+const catDance = document.querySelector('.cat-dance');
 
 //Data and preloads
 song.preload = 'auto';
 honk.playbackrate = 1.2;
 const flipValueArray = ['scaleX(1)', 'scaleX(-1)'];
 let i = 0;
-
 //Do's
 CHONKER.addEventListener('click', () => {
   song.play();
+  chonkHeader.classList.add('orchid-chonker');
+  topContainer.classList.add('top-slider');
+  bottomContainer.classList.add('bottom-slider');
   setTimeout(() => {
     chonkHeader.classList.add('spinny-Chonker');
   }, 6187.5);
@@ -23,6 +29,11 @@ CHONKER.addEventListener('click', () => {
     CHONKER.classList.add('bouncy-Chonker');
   }, 11812.5);
   setTimeout(() => {
+    dogeDance.classList.add('dancer');
+    catDance.classList.add('dancer');
+  }, 35625);
+  setTimeout(() => {
+    chonkHeader.classList.remove('orchid-chonker');
     chonkHeader.classList.remove('spinny-Chonker');
     CHONKER.classList.remove('bouncy-Chonker');
   }, 162000);
